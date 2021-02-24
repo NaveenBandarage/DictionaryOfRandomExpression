cf39b242d152f15b35db1f5f91665b0485bb3a96
Naveen Bandarage <thebanterage@gmail.com>
2021-02-24 21:23:12 +1300

    api returns data to console

--- src/dictionaryContent.js
+++ src/dictionaryContent.js
@@ -1,8 +1,37 @@
 
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+
 //https://random-words-api.vercel.app/word - api to use
-function dictionaryContent() {
+function DictionaryContent() {
+  const [appState, setAppState] = useState({
+    loading: true,
+    repos: null,
+  });
+  useEffect(() => {
+    let data = null;
+    let promises = [];
+    const apiUrl = `https://random-words-api.vercel.app/word`;
+    axios.get(apiUrl).then((res) => {
+      console.log(res);
+      // res.data.forEach((id) => {
+      //   promises.push(
+      //     axios.get(
+      //       sanitizeUrl(
+      //         `https://hacker-news.firebaseio.com/v0/item/${id}​​​​​.json?print=pretty`
+      //       )
+      //     )
+      //   );
+      // });
+      // Promise.all(promises)
+      //   .then((res) => {
+      //     console.log(Object.entries(res));
+      //     setAppState({ loading: false, repos: res });
+      //   })
+      //   .catch((err) => console.log(err));
+    });
+  }, []);
   return (
     <div>
       <p>This is some test text</p>
@@ -10,5 +39,5 @@ function dictionaryContent() {
   );
 }
 
-export default dictionaryContent;
+export default DictionaryContent;
 
